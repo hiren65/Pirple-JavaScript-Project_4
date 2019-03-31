4fa76125d9e1e344e69b86821426f5ebb11b0b84
utkarsh patel <hirenkumar@me.com>
2019-03-31 18:03:20 +1100

    serverless function completed and uploded on aws cloud

---
+++ getsquare/handler.js
@@ -0,0 +1,18 @@
+console.log('Loading function');
+
+module.exports.getSquare = async (event, context) => {
+  //console.log('Received event:', JSON.stringify(event, null, 2));
+  console.log(`Value`, event.key1);
+  // console.log('value2 =', event.key2);
+  //console.log('value3 =', event.key3);
+
+  //return event.key1;  // Echo back the first key value
+  let mynum = parseInt(event.key1);
+  return   "Value square " + pp(mynum);
+
+  // throw new Error('Something went wrong');
+};
+let pp = function (num){
+  return num*num;
+};
+
